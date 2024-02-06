4bb65dd804123054d54401baae6a170cbb88829d
Dhananjay Dubey <dubeydhananjay9@gmail.com>
2024-02-05 19:25:43 -0500

    remove trailing spaces and white spaces

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,18 +1,15 @@
 '''My Calculator Test'''
 from calculator import add, subtract, multiply, divide
-
 def test_addition():
-    '''Test that addition function works '''    
+    '''Test that addition function works '''
     assert add(2,2) == 4
-
 def test_subtraction():
-    '''Test that subtraction function works '''    
+    '''Test that subtraction function works '''
     assert subtract(2,2) == 0
-    
 def test_multiply():
-    '''Test that multiplication function works '''    
+    '''Test that multiplication function works '''
     assert multiply(3,2) == 6
-    
 def test_divide():
-    '''Test that division function works '''    
+    '''Test that division function works '''  
     assert divide(8,2) == 4
+    
